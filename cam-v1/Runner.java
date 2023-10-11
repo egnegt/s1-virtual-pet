6f5f1bcdc28000d6379e1b433ab4f522d6d56b74
egnegt <egnegto24@ausdk12.org>
2023-10-11 11:30:53 -0700

    gfxgf

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,6 +1,22 @@
 public class Runner {
-    public static void main(String[] args) {
-        new VirtualPet();
+    public Runner(){
+        VirtualPet v = new VirtualPet();
+        v.exercise();
+        takeABeat(1000);
+        v.feed();
+        String ans = getAnswer("How are you");
+        if (ans.equalsIgnoreCase)
+    }
 
+    public void takeABeat(int milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch(Exception e){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new Runner();
     }
 }
